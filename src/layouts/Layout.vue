ac6790010d4be72139b8bbc22e1fd1fc57800ebc
Aldo Barbosa <aldobarbosaads@gmail.com>
2021-10-22 14:11:56 -0300

    upload files

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab
+          v-for="nav in navs"
+          :key="nav.index"
+          :to="nav.to"
+          :icon="nav.icon"
+          :label="nav.label"
+          exact
+        />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8">Navigation</q-item-label>
+
+        <q-item
+          v-for="nav in navs"
+          v-bind:key="nav.index"
+          :to="nav.to"
+          exact
+          clickable
+        >
+          <q-item-section avatar>
+            <q-icon :name="nav.icon" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ nav.label }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+  data() {
+    return {
+      leftDrawerOpen: false,
+      navs: [
+        {
+          label: "Todo",
+          icon: "list",
+          to: "/",
+        },
+        {
+          label: "Settings",
+          icon: "settings",
+          to: "/settings",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+      <p>Settings Page</p>
+
+  </q-page>
+</template>
+
+<script>
+
+  export default {
+    
+  }
+  
+</script>
+
+<style>
+ 
+</style>
